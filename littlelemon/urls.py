e3192f40ef3e56c30e1576a625cea48d37817920
mtalirfan <mtalirfan@gmail.com>
2023-07-12 19:56:54 +0500

    evaluate

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for littlelemon project.
+"""littlelemon URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.authtoken.views import obtain_auth_token #import obtain_auth_token view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
-    # path('home/', include('home.urls'),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token),
 ]
